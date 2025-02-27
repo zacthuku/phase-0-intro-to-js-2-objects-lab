357543ac236dbcec12de1f776d72ebf39ad29731
Zacharia Ndinguri <zacthuku@gmail.com>
2025-02-27 10:06:36 +0300

    complete-lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+let employee ={
+    "name": 'Sam',
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {"name": 'Sam' , ['streetAddress']: '11 Broadway' };
+ }
+ function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee['streetAddress']= '12 Broadway'
+    return employee
+ }
+ function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = {};  
+    delete newEmployee['streetAddress'];  
+    return newEmployee;  
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];  
+    return employee;
+ }
+
